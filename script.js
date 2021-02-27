c5c2c46467ee5e48d8e94d22adeed1e669e5b7b4
B Brucato <bebrucato@gmail.com>
2021-02-27 09:43:06 -0800

    function to display current weather attributes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //open string for "city" so it can take on the value of any location
+//global vars for different weather attributes
 var city ="";
 var citySearch = $("#search-city");
 var searchButton = $("#search-button");
@@ -9,6 +10,7 @@ var currentWind = $("#wind-speed");
 var currentUV = $("#uv-index");
 var sCity = [];
 
+//To find the city
 function find(c){
     for (var i=0; i<sCity.length; i++){
         if(c.toUpperCase()===sCity[i]){
@@ -20,11 +22,65 @@ function find(c){
 
 var APIKey="ba16492eb916e36730d06ac08e215281";
 
+//Displaying the overall weather
 function displayWeather(event){
     event.preventDefault();
-    if(searchCity.val().trim()!==""){
-        city=searchCity.val().trim();
+    if(citySearch.val().trim()!==""){
+        city=citySearch.val().trim();
         currentWeather(city);
     }
 }
-console.log(displayWeather)
+
+//Gathering the current weather
+function currentWeather(city){   
+//The url to "get" the weather data from the api for the city
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+    console.log(response);
+
+//Var for the weather icon/var for url to access icons/access dates
+        var weathericon= response.weather[0].icon;
+        var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+        var date=new Date(response.dt*1000).toLocaleDateString();
+        $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
+
+//Converting the temp to farenheit
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $(currentTemp).html((tempF).toFixed(2)+"&#8457");
+
+//Humidity
+        $(currentHumidity).html(response.main.humidity+"%");
+
+//Wind speed and convert to MPH
+        var ws=response.wind.speed;
+        var windsmph=(ws*2.237).toFixed(1);
+        $(currentWind).html(windsmph+"MPH");
+       
+//UV display/using coordinagtes as a paramenter to gather the data
+        UVIndex(response.coord.lon,response.coord.lat);
+        forecast(response.id);
+        if(response.cod==200){
+            sCity=JSON.parse(localStorage.getItem("cityname"));
+            console.log(sCity);
+            if (sCity==null){
+                sCity=[];
+                sCity.push(city.toUpperCase()
+                );
+                localStorage.setItem("cityname",JSON.stringify(sCity));
+                addToList(city);
+            }
+            else {
+                if(find(city)>0){
+                    sCity.push(city.toUpperCase());
+                    localStorage.setItem("cityname",JSON.stringify(sCity));
+                    addToList(city);
+                }
+            }
+        }
+
+    });
+}
+
